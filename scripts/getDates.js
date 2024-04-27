4a0d13147fd2254f50cae38caafdefe3bae79b2b
kaymanda <mandaks86@gmail.com>
2024-04-27 13:44:07 -0600

    change js

--- scripts/getDates.js
+++ scripts/getDates.js
@@ -1,8 +1,6 @@
 //copyright current year and last modified
 
-const currentYear = new Date().getFullYear();
-
-document.getElementById("year").innerHTML = currentYear;
+document.getElementById("year").innerHTML = new Date().getFullYear();
 
 const lastModified = document.lastModified;
 document.getElementById("lastModified").innerHTML = 'Last Modified: ' +lastModified;
