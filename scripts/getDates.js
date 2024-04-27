6cf2b4639ce663a481f7c6d6245cecd57b1fad73
kaymanda <mandaks86@gmail.com>
2024-04-27 13:08:48 -0600

    update three

--- scripts/getDates.js
+++ scripts/getDates.js
@@ -3,7 +3,5 @@
 const currentYear = new Date().getFullYear();
 document.querySelector("#year").textContent = `${currentYear}`;
 
-alert(document.lastModified);
-
-//const madeChanges = document.lastModified;
-//document.getElementById("lastModified").innerHTML = `Last Modified: ${madeChanges}`;
+const madeChanges = document.lastModified;
+document.getElementById("lastModified").innerHTML = `Last Modified: ${madeChanges}`;
