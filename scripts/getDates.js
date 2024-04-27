2effca36f323291fd707a0fb280e8af714fc3eb6
kaymanda <mandaks86@gmail.com>
2024-04-27 12:52:49 -0600

    week 1

---
+++ scripts/getDates.js
@@ -0,0 +1,9 @@
+//copyright current year and last modified
+
+const currentYear = new Date().getFullYear();
+document.querySelector("#year").textContent = `${currentYear}`;
+
+alert(document.lastModified);
+
+//const madeChanges = document.lastModified;
+//document.getElementById("lastModified").innerHTML = `Last Modified: ${madeChanges}`;
