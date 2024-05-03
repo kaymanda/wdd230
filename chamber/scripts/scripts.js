6c896a903ee53370abb6223340de64754deacb85
kaymanda <mandaks86@gmail.com>
2024-05-03 11:18:32 -0600

    Start of chamber project

---
+++ chamber/scripts/scripts.js
@@ -0,0 +1,7 @@
+const hamburgerButton = document.querySelector("#theButton");
+const navMenu = document.querySelector(".menu");
+
+hamburgerButton.addEventListener("click", () => {
+    navMenu.classList.toggle("open");
+    hamburgerButton.classList.toggle("open");
+});
