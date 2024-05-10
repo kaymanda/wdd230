85e04c5645fa2f69dfe4afafc577d3a94e8630ad
kaymanda <mandaks86@gmail.com>
2024-05-10 14:44:46 -0600

    update two of the day

--- chamber/scripts/scripts.js
+++ chamber/scripts/scripts.js
@@ -5,3 +5,17 @@ hamburgerButton.addEventListener("click", () => {
     navMenu.classList.toggle("open");
     hamburgerButton.classList.toggle("open");
 });
+
+const visitsDisplay = document.querySelector(".visits");
+let numVisits = Number(window.localStorage.getItem("numVisits-1s")) || 0;
+if (numVisits !== 0) {
+    visitsDisplay.textContent = numVisits;
+} else {
+    visitsDisplay.textContent = `Welcome! Let us know if you have any questions.`;
+}
+
+document.querySelector('.today').textContent = Date.now();
+
+numVisits++;
+
+localStorage.setItem("numVisits-1s", numVisits);
