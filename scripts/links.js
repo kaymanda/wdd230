e364ba12323297ce5f4c0d30ba091c52647eb5e5
kaymanda <mandaks86@gmail.com>
2024-05-23 23:49:33 -0600

    added

--- scripts/links.js
+++ scripts/links.js
@@ -5,7 +5,25 @@ const linksURL = "https://kaymanda.github.io/wdd230/data/links.json";
 async function getLinks() {
     const response = await fetch(linksURL);
     const data = await response.json;
-    console.log(data);
+    displayLinks(data);
 }
+
+const displayLinks = (weeks) => {
+    weeks.forEach((week) => {
+        const li = document.createElement("li");
+        li.textContent = `${week.week} :`;
+
+        links.forEach((link) => {
+            const a = document.createElement("a");
+            a.setAttribute("href",link.url);
+            a.textContent = `${link.title}`;
+
+            li.appendChild(a);
+            URL.appendChild(li);
+        
+        });
+
+    });
+};
 
 getLinks();
