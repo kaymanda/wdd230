d45752a93cf4a92c8dee7e5bf4f736f776aa8beb
kaymanda <mandaks86@gmail.com>
2024-05-29 01:01:15 -0600

    trying2

--- chamber/scripts/scripts.js
+++ chamber/scripts/scripts.js
@@ -30,3 +30,37 @@ function setTimeStamp() {
 }
 setTimeStamp();
 
+//Weather
+
+
+const currentTemp = document.querySelector("#current-temp");
+const weatherIcon = document.querySelector("#weather-icon");
+const captionDesc = document.querySelector("figcaption");
+
+const url = "https://api.openweathermap.org/data/2.5/weather?lat=40.53&lon=-112.30&units=imperial&appid=64aa0d0fe4df0547171dff1747aa255f";
+
+async function apiFetch() {
+    try{
+        const response = await fetch(url);
+        if (response.ok) {
+            const data = await response.json();
+            displayResults(data);
+        } else {
+            throw Error(await response.text());
+        }
+    } catch(error) {
+        console.log(error);
+    }
+}
+
+apiFetch();
+
+function displayResults(data) {
+    currentTemp.innerHTML = `${data.main.temp}&deg;F`;
+    const iconsrc = `https://openweathermap.org/img/w/${data.weather-card[0].icon}.png`;
+    let desc = data.weather-card[0].description;
+    weatherIcon.setAttribute("src", iconsrc);
+    weatherIcon.setAttribute("alt", desc);
+    captionDesc.textContent = desc;
+}
+
