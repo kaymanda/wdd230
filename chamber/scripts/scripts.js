0cd12a69cfe23c7e461586d3b3d95efdd85c6ed2
kaymanda <mandaks86@gmail.com>
2024-06-04 16:22:29 -0600

    change

--- chamber/scripts/scripts.js
+++ chamber/scripts/scripts.js
@@ -10,7 +10,7 @@ hamburgerButton.addEventListener("click", () => {
 const visitsDisplay = document.querySelector(".visits");
 let numVisits = Number(window.localStorage.getItem("numVisits-1s")) || 0;
 if (numVisits !== 0) {
-    visitsDisplay.textContent = numVisits;
+    visitsDisplay.innerHTML = numVisits;
 } else {
     visitsDisplay.textContent = `Welcome! Let us know if you have any questions.`;
 }
@@ -26,15 +26,6 @@ localStorage.setItem("numVisits-1s", numVisits);
 const banner = document.querySelector('#banner');
 var DayOfWeek = new Date().getDay();
 
-if (DayOfWeek == 1 || DayOfWeek == 2 || DayOfWeek == 3) {
-    banner.style.display = "block";
 
-} else {
-    banner.style.display = "none";
-}
 
-const bannerClose = document.querySelector('#close-banner');
 
-bannerClose.addEventListener('click', function() {
-    banner.style.display = "none";
-});
