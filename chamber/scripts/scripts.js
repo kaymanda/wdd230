308bd64398bb9767bea5d72bb17ff0b212b46ced
kaymanda <mandaks86@gmail.com>
2024-06-04 01:34:59 -0600

    trying new things out

--- chamber/scripts/scripts.js
+++ chamber/scripts/scripts.js
@@ -6,20 +6,33 @@ hamburgerButton.addEventListener("click", () => {
     hamburgerButton.classList.toggle("open");
 });
 
-const visitsDisplay = document.querySelector(".visits");
-let numVisits = Number(window.localStorage.getItem("numVisits-1s")) || 0;
-if (numVisits !== 0) {
-    visitsDisplay.textContent = numVisits;
+/* Last Visited */
+
+const msDays = 84600000; // converting ms to days
+
+if (localStorage.getItem("lastDateVisit")) {
+  const lastDateVisit = new Date(localStorage.getItem("lastDateVisit")).getTime();
+  const today = Date.now();
+  let daysDiffer = (today - lastDateVisit) / msDays;
+  let message = "";
+  if (daysDiffer < 1) {
+    message = "Back so soon! Awesome!";
+  }else if (daysDiffer >= 1) {
+    if (daysDiffer > 1 && daysDiffer < 2) {
+      message = `Your last visit was ${Math.floor(daysDiffer)} day ago`;
+    } else {
+      message = `Your last visit was ${Math.floor(daysDiffer)} days ago`;
+    }
+  }
+
+  const visitDate = document.querySelector("#last-visit");
+  visitDate.textContent = message;
+  localStorage.setItem("lastDateVisit", todaysDate);
 } else {
-    visitsDisplay.textContent = `Welcome! Let us know if you have any questions.`;
+  document.getElementById("last-visit").textContent =
+    "Welcome! Let us know if you have any questions.";
 }
 
-
-document.querySelector('.today').textContent = Date.now();
-
-numVisits++;
-
-localStorage.setItem("numVisits-1s", numVisits);
 
 /*Time Stamp*/
 
@@ -30,37 +43,5 @@ function setTimeStamp() {
 }
 setTimeStamp();
 
-//Weather
 
-
-const currentTemp = document.querySelector("#current-temp");
-const weatherIcon = document.querySelector("#weather-icon");
-const captionDesc = document.querySelector("figcaption");
-
-const url = "https://api.openweathermap.org/data/2.5/weather?lat=40.53&lon=-112.30&units=imperial&appid=64aa0d0fe4df0547171dff1747aa255f";
-
-async function apiFetch() {
-    try{
-        const response = await fetch(url);
-        if (response.ok) {
-            const data = await response.json();
-            displayResults(data);
-        } else {
-            throw Error(await response.text());
-        }
-    } catch(error) {
-        console.log(error);
-    }
-}
-
-apiFetch();
-
-function displayResults(data) {
-    currentTemp.innerHTML = `${data.main.temp}&deg;F`;
-    const iconsrc = `https://openweathermap.org/img/w/${data.weather-card[0].icon}.png`;
-    let desc = data.weather-card[0].description;
-    weatherIcon.setAttribute("src", iconsrc);
-    weatherIcon.setAttribute("alt", desc);
-    captionDesc.textContent = desc;
-}
 
---
+++ chamber/scripts/weather.js
@@ -0,0 +1,24 @@
+const currentTemp = document.querySelector("#current-temp");
+const weatherIcon = document.querySelector("#weather-icon");
+const captionDesc = document.querySelector("figcaption");
+
+const url = "https://api.openweathermap.org/data/2.5/weather?lat=40.53&lon=-112.30&units=imperial&appid=64aa0d0fe4df0547171dff1747aa255f";
+
+async function apiFetch() {
+    const response = await fetch(url);
+    const data = await response.json();
+  
+    displayResults(data);
+  }
+  
+  function displayResults(data) {
+    currentTemp.textContent = `${data.main.temp} °F`;
+    weatherIcon.src = `https://openweathermap.org/img/wn/${data.weather[0].icon}.png`;
+    captionDesc.textContent = data.weather[0].description;
+  }
+  
+  apiFetch();
+  
+  
+  
+  
